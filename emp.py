e48ac2c3b947156e86573d9da5ad0c68a98a3a49
blackdragon0465-coder <blackdragon0465@gmail.com>
2025-11-20 15:29:36 +0530

    Update argument count and add experience parameter

--- emp.py
+++ emp.py
@@ -1,15 +1,17 @@
 import sys
 
-if len(sys.argv) != 4:
-    print("Usage: python emp.py <name> <id> <salary>")
+if len(sys.argv) != 5:
+    print("Usage: python emp.py <name> <id> <salary> <experiance>")
     sys.exit(1)
 
 script_name = sys.argv[0]
 name = sys.argv[1]
 id = sys.argv[2]
 salary = sys.argv[3]
+experiance = sys.argv[4]
 
 print("Script Name:", script_name)
 print("Name:", name)
 print("ID Number:", id)
 print("Salary:", salary)
+print("Experiance:", experiance)
